0961641b301504dce85552d6d8812c7d2d64990d
lchiapparo-novaa <lchiapparo@novaa-tech.com>
2023-03-08 15:43:48 +0000

    .gitignore + endpoint with lambda invoke added

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var AWS = require('aws-sdk');
+require('dotenv').config();
+
+AWS.config.region = 'eu-west-3';
+
 var app = express();
 var port = process.env.PORT || 3000;
 
+var lambda = new AWS.Lambda();
+
 app.get('/', function (req, res) {
     res.send('Sample Node.js App');
+});
+
+app.get('/test-lambda', function (req, res) {
+    var params = {
+        FunctionName: process.env.AWS_HELLO_WORLD_LAMBDA,
+        InvocationType: 'RequestResponse',
+    };
+
+    lambda.invoke(params, function (err, data) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(data.Payload);
+        }
+    })
 });
 
 app.listen(port, function () {
